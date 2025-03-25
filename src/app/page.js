53d243c7e54391f009c076875f4791f634695b30
Bimla-jain-555 <vj13798@gmail.com>
2025-03-25 14:56:47 +0530

    change faq component name

--- src/app/page.js
+++ src/app/page.js
@@ -2,12 +2,12 @@ import BreakingNews from '@/components/BreakingNews'
 import Hero from '@/components/Hero'
 import Minting from '@/components/Minting'
 import React from 'react'
-import Faq from '@/components/Faq'
 import Footer from '@/components/Footer'
 import Header from '@/components/common/Header'
 import Roadmap from '@/components/Roadmap'
 import LatestTweets from '@/components/LatestTweets'
 import Team from '@/components/team'
+import Faq from '@/components/faq'
 const page = () => {
   return (
     <div>

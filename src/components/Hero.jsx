653ac3e380e84fdbb827cc1eb7b5a1209765ef6a
Bimla-jain-555 <vj13798@gmail.com>
2025-03-25 15:32:12 +0530

    add 2nd font family

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,7 +3,7 @@ import Image from 'next/image'
 const Hero = () => {
   return (
     <div className=' pt-[25px] max-w-[1920px] mx-auto'>
-      <h1 className='font-normal xl:text-[157px] lg:text-[120px] md:text-[80px] text-4xl text-center leading-[100%]'>THE HUSTLIN’ HARDOS.</h1>
+      <h1 className='font-normal xl:text-[157px] lg:text-[120px] md:text-[80px] text-4xl ff-moderno text-center leading-[100%]'>THE HUSTLIN’ HARDOS.</h1>
       <Image className='w-full border-y-2 border-black' src="/assets/images/png/hero-image.png" alt='hero-image' width={1440} height={476}/>
     </div>
   )

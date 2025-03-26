f02e00d5e50a7b2bd9d56cbe473fc1178f147c34
Bimla-jain-555 <vj13798@gmail.com>
2025-03-26 14:43:03 +0530

    change border radius if button in responsive

--- src/components/common/Header.jsx
+++ src/components/common/Header.jsx
@@ -64,7 +64,7 @@ const Navbar = () => {
               </Link>
             ))}
           </div>
-          <button className="bg-yellow-400 text-black px-4 py-2 font-semibold rounded border border-black hover:bg-yellow-500 transition-all duration-500 mt-4">
+          <button className="bg-yellow-400 text-black px-4 py-2 font-semibold border border-black hover:bg-yellow-500 transition-all duration-500 mt-4">
             CONNECT WALLET
           </button>
         </div>
